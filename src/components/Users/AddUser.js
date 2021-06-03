ab12f07ccc7a037c2db4e11f2e7fba7dde1d15ab
Mitch <mitch@Tielman-Macbook.local>
2021-06-03 18:45:10 +0200

    added Card wrapper component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
+import Card from "../UI/Card";
 
-const AddUser = props => {
-
-    const addUserHandler = (event) => {
-        event.preventDefault();
-        
-    }
-    return(
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
-            <label htmlFor="age">Age (Years)</label>
-            <input type="number" id="age" />
-            <button type="submit">Add User</button>
-        </form>
-    );
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
