3af083ae125e2d1cd2c10234b4eaacc8672c732a
Mitch <mitch@Tielman-Macbook.local>
2021-06-05 13:44:06 +0200

    added onAddUser event
    this event manages added users to the Users list, it uses preUsersList to retain previous User data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 const App = () => {
-  const users = [{ name: "Mitchell", age: 21 }];
+  const [usersList, setUsersList] = useState([{ name: "Mitchell", age: 21 }]);
+  const addUserHandler = (uName, uAge) => {
+      setUsersList((prevUsersList)=>{
+          return [...prevUsersList, { name: uName, age: uAge}]
+      })
+  };
   return (
     <div>
-      <AddUser />
-      <UsersList users={users} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 };
